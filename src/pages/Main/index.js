7fa46f0caba59b41abfd4cd33e0726e34370fac1
Francinildo <francis584@gmail.com>
2019-09-13 17:53:31 -0300

    Style of the Form Done.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
   return (
-    <View>
-      <Text>Sou a Main</Text>
-    </View>
+    <Container>
+      <Form>
+        <Input autoCorrect={false} autoCapitalize="none" />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 Main.navigationOptions = {

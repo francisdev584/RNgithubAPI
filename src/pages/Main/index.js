11c9d8aeacf9047d41902a0adbe28051f4122823
Francinildo <francis584@gmail.com>
2019-09-16 09:57:04 -0300

    Get Data from github API with axios - Done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Keyboard} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import {Container, Form, Input, SubmitButton} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input autoCorrect={false} autoCapitalize="none" />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const {newUser, users} = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton>
+            <Icon
+              name="add"
+              size={20}
+              color="#FFF"
+              onPress={this.handleAddUser}
+            />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 Main.navigationOptions = {
   title: 'Usuários',

6a58c195b5cba8a78a3baa2e9d9f1c2d31dcc796
Francinildo <francis584@gmail.com>
2019-09-16 17:17:46 -0300

    get favorites from github - Done.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
+import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({navigation}) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  // eslint-disable-next-line react/static-property-placement
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+    const response = await api.get(`users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {stars} = this.state;
+    console.tron.log(stars);
+    return <View />;
+  }
 }

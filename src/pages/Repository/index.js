0ec02ad84e8d2809f3706ae878a8d770037e6ebc
Francinildo <francis584@gmail.com>
2019-10-15 09:13:27 -0300

    refact the Repository page and style

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,29 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {WebView} from 'react-native-webview';
-// import {View} from 'react-native';
+import {Browser} from './styles';
 
-// import { Container } from './styles';
-
-export default class Repository extends Component {
-  static navigationOptions = ({navigation}) => ({
-    title: navigation.getParam('repository').name,
-  });
-
-  // eslint-disable-next-line react/static-property-placement
-  static propTypes = {
-    navigation: PropTypes.shape({
-      getParam: PropTypes.func,
-    }).isRequired,
-  };
-
-  render() {
-    const {navigation} = this.props;
-    return (
-      <WebView
-        source={{uri: navigation.getParam('repository').html_url}}
-        style={{flex: 1}}
-      />
-    );
-  }
+export default function Repository({navigation}) {
+  const repository = navigation.getParam('repository');
+  return <Browser source={{uri: repository.html_url}} />;
 }
+
+Repository.navigationOptions = ({navigation}) => ({
+  title: navigation.getParam('repository').name,
+});
+
+// eslint-disable-next-line react/static-property-placement
+Repository.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
